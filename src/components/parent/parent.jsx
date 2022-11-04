9279250adf07a313fbcbd7f83eacfff3848aeca9
Patel <alpna.patel@capgemini.com>
2022-11-04 15:09:10 +0530

    final

--- src/components/parent/parent.jsx
+++ src/components/parent/parent.jsx
@@ -1,17 +1,16 @@
-import { getValue } from '@testing-library/user-event/dist/utils'
 import React, { useState } from 'react'
 import SenderChild from '../sender-child/sender-child'
 import ReceiverChild from '../receiver-child/receiver-child'
 
 const Parent = () => {
-  const [value, setValue] = useState(0);
+  const [counter, setCounter] = useState(0);
   const getValueFromSenderChild = () => {
-    setValue(value + 1);
+    setCounter(counter + 1 );
   };
   return (
     <>
       <SenderChild getValueFromSenderChild={getValueFromSenderChild} />
-      <ReceiverChild count={value} />
+      <ReceiverChild count={counter} />
     </>
   )
 }
